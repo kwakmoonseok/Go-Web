482ed56aa47fd21f4b98a02c50622340abd8c52b
kwakmoonseok <kms34510@naver.com>
2021-04-21 20:11:48 +0900

    RestAPI Post
    6강 수강

--- restapi/myapp/app_test.go
+++ restapi/myapp/app_test.go
@@ -1,9 +1,12 @@
 package myapp
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -46,5 +49,33 @@ func TestGetUserInfo(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(http.StatusOK, res.StatusCode)
 	data, _ := ioutil.ReadAll(res.Body)
-	assert.Contains(string(data), "User Id:89")
+	assert.Contains(string(data), "No User Id:89")
+}
+
+func TestCreateUserInfo(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+
+	res, err := http.Post(ts.URL + "/users", "application/json", 
+		strings.NewReader(`{"first_name" : "kinder", "last_name": "kwak", "email" : "example@gmail.com"}`))
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, res.StatusCode)
+
+	user := new(User)
+	err = json.NewDecoder(res.Body).Decode(user)
+	assert.NoError(err)
+	assert.NotEqual(0, user.ID)
+
+	id := user.ID
+	res, err = http.Get(ts.URL + "/users/" + strconv.Itoa(id))
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, res.StatusCode)
+
+	user2 := new(User)
+	err = json.NewDecoder(res.Body).Decode(user2)
+	assert.NoError(err)
+	assert.Equal(user.ID, user2.ID)
+	assert.Equal(user.FirstName, user2.FirstName)
 }
